7f58fc43f6620858560f24e6fc0c6111afbeb9c5
Vitaliy S <sofronov.vit@gmail.com>
2023-10-14 20:40:28 +0400

    fill main module with initial files

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
-
+import { HeaderComponent } from './components/header/header.component';
+import { CommercialComponent } from './pages/commercial/commercial.component';
+import { MortgageComponent } from './pages/mortgage/mortgage.component';
+import { RentComponent } from './pages/rent/rent.component';
+import { SaleComponent } from './pages/sale/sale.component';
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, CommercialComponent, MortgageComponent, RentComponent, SaleComponent],
   imports: [CommonModule, MainRoutingModule]
 })
 export class MainModule {}
