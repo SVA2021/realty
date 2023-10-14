e17c87c928c652b67795bce9642389f9895798d7
Vitaliy S <sofronov.vit@gmail.com>
2023-10-14 19:34:34 +0400

    add base file structure and import all taiga modules in one file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgDompurifySanitizer } from '@tinkoff/ng-dompurify';
-import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER } from '@taiga-ui/core';
+import { TUI_SANITIZER, tuiSvgOptionsProvider } from '@taiga-ui/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ALL_TAIGA_UI_MODULES } from './helpers/taiga/all-taiga-modules';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, TuiRootModule, TuiDialogModule, TuiAlertModule],
-  providers: [{ provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, ALL_TAIGA_UI_MODULES],
+  providers: [
+    tuiSvgOptionsProvider({
+      path: 'https://taiga-ui.dev/assets/taiga-ui/icons'
+    }),
+    /**
+     * If you use unsafe icons or TuiEditor in your app
+     * Take a look at: https://github.com/taiga-family/ng-dompurify
+     * This library implements DOMPurify as Angular Sanitizer or Pipe.
+     * It delegates sanitizing to DOMPurify and supports the same configuration.
+     */
+    { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
